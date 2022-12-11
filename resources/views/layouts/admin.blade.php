c4b5bf7d4d737b64d38dd3ac70d93ec8fad71fe2
jherrera245 <245jh17@gmail.com>
2022-12-11 11:44:41 -0600

    Agregando ayuda de sistema

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -229,6 +229,13 @@
                                 </li>
                             </ul>
                         </li>
+
+                        <li class="nav-item">
+                            <a href="https://youtu.be/KP-yfkas0Fc" target="_blank" class="nav-link">
+                                <i class="nav-icon fas fa-info"></i>
+                                <p>Ayuda</p>
+                            </a>
+                        </li>
                         
                     </ul>
                 </nav>
